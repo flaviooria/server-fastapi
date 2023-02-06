792c31044ea5ec4067d82e289db11231ec943974
Flavio Oria <flavio.oriap@gmail.com>
2023-02-06 09:29:18 +0100

    Creando ficheros estaticos

--- routers/products.py
+++ routers/products.py
@@ -6,8 +6,8 @@ from entities import ProductEntity
 # El prefijo sirve para no tener que añadir el /products en cada route que añadieramos
 # y el tags, servira para darle una categoría y en la documentación se muestre de esa manera
 router = APIRouter(prefix='/products',tags=['products'])
-products = read_json('products.json')
+products: list[ProductEntity] = read_json('products.json')
 
-@router.get('/',status_code=200,response_model=list(ProductEntity))
+@router.get('/',status_code=200,response_model=list[ProductEntity])
 def getProducts():
     return products
--- test_api.py
+++ test_api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
+from fastapi.staticfiles import StaticFiles
 
 from routers import users,products
 
@@ -8,6 +9,14 @@ app = FastAPI()
 # Add routes
 app.include_router(users.router)
 app.include_router(products.router)
+
+# Add static files
+# Para añadir archivos estaticos se utiliza el método mount. Tiene 3 párametros
+# path: Nombre del path que usara la api,
+# app: Que sera en este caso la clase StaticFiles, donde obtendra nuestros archivos estaticos
+# name: Es el nombre que recibira por defecto
+app.mount('/static',StaticFiles(directory='static',html=True),'static')
+
 
 @app.get('/',status_code=200)
 async def root():
